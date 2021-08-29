e68671b252844f0439ba331de408a36a2cddbe30
shrkdvy <shrkdvy123@gmail.com>
2021-08-29 12:32:55 +0900

    홈 contact버튼 클릭시 contact화면으로 이동

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //스크롤 내릴시 보이는 페이지에 냅바 고정
 const navbar = document.querySelector('#Navbar');
-window.addEventListener('scroll', (event) => {
+window.addEventListener('scroll', () => {
   if (window.pageYOffset >= navbar.getBoundingClientRect().height) {
     navbar.classList.add('dark-mode');
   } else {
@@ -10,11 +10,8 @@ window.addEventListener('scroll', (event) => {
   }
 });
 
-//
-
 // 냅바 메뉴 누르면 해당 메뉴로 스크롤 이동
 const navbarMenu = document.querySelector('.navbar__menu');
-
 navbarMenu.addEventListener('click', (event) => {
   const link = event.target.dataset.link;
   scrollIntoView(link);
@@ -27,3 +24,10 @@ function scrollIntoView(selector) {
     behavior: 'smooth',
   });
 }
+
+//홈화면 버튼클릭시 contact로 이동
+const contactBtn = document.querySelector('.home__btn');
+contactBtn.addEventListener('click', (event) => {
+  const link = event.target.dataset.link;
+  scrollIntoView(link);
+});
