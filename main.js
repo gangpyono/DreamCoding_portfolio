3479ee3cd34a470c215fc4bb9b7a9d9e6c4ff0da
shrkdvy <shrkdvy123@gmail.com>
2021-09-04 11:30:34 +0900

    navbar 너비 및 반응형 수정

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ window.addEventListener('scroll', () => {
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const link = event.target.dataset.link;
+  if (link == null) return;
   scrollIntoView(link);
   navbarMenu.classList.remove('active');
 });
